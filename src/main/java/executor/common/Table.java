1af91b5429449531cccd29a1e4d470dc6091998e
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2025-09-09 10:52:25 +0800

    初步添加索引

--- src/main/java/executor/common/Table.java
+++ src/main/java/executor/common/Table.java
@@ -3,6 +3,7 @@ package executor.common;
 import executor.common.Record;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.function.Predicate;
 
 public interface Table {
@@ -10,6 +11,8 @@ public interface Table {
     CloseableIterator<executor.common.Record> scan();
     void insert(executor.common.Record record);
     void delete(Predicate<Record> condition);
+    List<Index> getIndexes();   //获得表上所有索引
+    Index getIndex(String indexName);   //获得特定名称的索引
 
     // 元数据
     TableSchema getSchema();

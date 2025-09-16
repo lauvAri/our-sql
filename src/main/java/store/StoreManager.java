b0f685688351765b3314043cb7cf58f14cbef5aa
lauvAri <198719993@qq.com>
2025-09-16 16:32:01 +0800

    修复系统å表读取空指针的问题

--- src/main/java/store/StoreManager.java
+++ src/main/java/store/StoreManager.java
@@ -164,7 +164,10 @@ public class StoreManager {
 
     public void loadAllTables() {
         Table table = loadTable(SystemCatalog.CATALOG_TABLE_NAME);
-        tables.put(SystemCatalog.CATALOG_TABLE_NAME, table);
+        if (table != null) {
+            tables.put(SystemCatalog.CATALOG_TABLE_NAME, table);
+        }
+
 //        if (table != null) {
 //            List<Record> records = table.getAllRecords();
 //            for (Record record : records) {
